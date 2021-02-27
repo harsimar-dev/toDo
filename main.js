c691bbed91de215cbdb94edda65b0b5e62c55bf5
harsimar-dev <77212792+harsimar-dev@users.noreply.github.com>
2021-02-27 13:15:39 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField')
+
+
+
+addToDoButton.addEventListener('click' , function(){
+
+  var paragraph = document.createElement('p')
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+
+  paragraph.addEventListener('click', function(){
+
+       paragraph.style.textDecoration = "line-through";
+  })
+
+  paragraph.addEventListener('dblclick',function(){
+
+       toDoContainer.removeChild(paragraph);
+  })
+
+  updatelist();
+
+})
+
+function updatelist(){
+
+var itemsText = document.querySelectorAll('p');
+var items = [];
+
+itemsText.forEach(item=> {
+  items.push(item.value);
+})
+
+localStorage.setItem("items",JSON.stringify(items));
+
+}
