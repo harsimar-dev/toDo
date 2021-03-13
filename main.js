86507d24c41b3c58011c61abddf0a5884599dd46
harsimar-dev <77212792+harsimar-dev@users.noreply.github.com>
2021-03-13 13:00:28 -0800

    Credits to thecodingpie

--- main.js
+++ main.js
@@ -1,39 +1,80 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField')
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const todoitems = document.querySelector('.todoitems');
 
 
+let todos = [];
 
-addToDoButton.addEventListener('click' , function(){
+form.addEventListener('submit', function(event){
 
-  var paragraph = document.createElement('p')
-  paragraph.innerText = inputField.value;
-  toDoContainer.appendChild(paragraph);
-  inputField.value = "";
+event.preventDefault();
+addTodo(input.value);
 
-  paragraph.addEventListener('click', function(){
+});
 
-       paragraph.style.textDecoration = "line-through";
+function addTodo(item){
+
+if (item !== ''){
+
+  const todo = {
+     id: Date.now(),
+     name: item,
+     completed: false
+  };
+
+todos.push(todo);
+addToLocalStorage(todos);
+
+input.value = '';
+
+}
+
+}
+
+function renderTodos(todos){
+
+todoitems.innerHTML = '';
+
+todos.forEach(function(item){
+
+     const checked = item.completed? 'checked': null;
+
+      const li = document.createElement('li');
+      li.setAttribute('class','item');
+      li.setAttribute('data-key',item.id);
+
+      if(item.completed === true){
+        li.classlist.add('checked');
+      }
+
+      li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${checked}>
+      ${item.name}
+      <button class="delete-button">X</button>
+    `;
+
+     todoitems.append(li);
+});
+
+}
+
+function addToLocalStorage(todos){
+  localStorage.setItem('todos',JSON.stringify(todos));
+  renderTodos(todos);
+}
+
+function toggle(id){
+  todos.forEach(function(item){
+    if(item.id == id){
+      item.completed = !item.completed;
+    }
+  });
+
+  addToLocalStorage(todos);
+}
+
+function deleteTodo(id){
+  todos = todos.filter(function(item){
+    return item.id != id;
   })
-
-  paragraph.addEventListener('dblclick',function(){
-
-       toDoContainer.removeChild(paragraph);
-  })
-
-  updatelist();
-
-})
-
-function updatelist(){
-
-var itemsText = document.querySelectorAll('p');
-var items = [];
-
-itemsText.forEach(item=> {
-  items.push(item.value);
-})
-
-localStorage.setItem("items",JSON.stringify(items));
-
 }
